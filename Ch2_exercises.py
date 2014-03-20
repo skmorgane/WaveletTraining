6778042d52c2e62eb59d82626ce08c35b25b67a8
Morgan Ernest <morgan@weecology.org>
2014-03-20 11:09:37 -0400

    Changed function name because name had changed in source code for function

--- Ch2_exercises.py
+++ Ch2_exercises.py
@@ -8,7 +8,7 @@ import subsignal_generator as haar
 f_21A = [2,2,2,4,4,4]
 a_21A, d_21A = haar.generate_fluctuation_trend_subsignals(f_21A, 1)
 
-#Example2.1.D: Computing first  trend subsignal and first fluctuations for the following signals
+#Example2.1.D: Computing first trend subsignal and first fluctuations for the following signals
 f_21D_a = [2,4,6,6,4,2]
 f_21D_b = [-1,1,2,-2,4,-4,2,2]
 f_21D_c = [1,2,3,3,2,2,1,1]
